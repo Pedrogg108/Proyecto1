fb5d503b83550ea953586810734c123d0e4d611c
peter alancay <pedroalancay2018@gmail.com>
2026-04-20 11:04:06 -0300

    cambios2

---
+++ src/app/models/juegos.ts
@@ -0,0 +1,9 @@
+export interface Juegos {
+    id:number;
+    nombre:string;
+    descripcion:string;
+    precio:number;
+    img?:string;
+    alt?:string;
+
+}
--- src/app/paginas/productos/productos.ts
+++ src/app/paginas/productos/productos.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { Juegos } from '../../models/juegos';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-productos',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './productos.html',
   styleUrl: './productos.css',
 })
-export class Productos {}
+export class Productos {
+  public infoJuegos: Juegos[];
+  constructor() {
+    this.infoJuegos = [{
+      id: 1,
+      nombre: "Samsung A06",
+      descripcion: "64GB/4GB Ram",
+      precio: 456000,
+      img: "../../../assets/DBZ.jpg",
+      alt: "DBZ"
+    }
+    ]
+  }
+  juegoSeleccionado: any = null
+  verMas(juegos: any) {
+    this.juegoSeleccionado = juegos
+  }
+}
